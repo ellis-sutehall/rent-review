2e1f319b1098ccac6387fc8b2d9174a76466fc19
Ellis Sutehall <ellissutehall@gmail.com>
2020-04-02 21:11:42 +0100

    Adds review section. Adds homepage background image. Adds homepage search styles. Adds Heading 1 component.

---
+++ src/components/headingOne.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const HeadingOne = props => {
+  return <h1 className="title is-1">{props.pageTitle}</h1>
+}
+
+export default HeadingOne
--- src/components/homeHeroBody.js
+++ src/components/homeHeroBody.js
@@ -5,6 +5,17 @@ import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"
 
 const HomeHeroBody = () => {
   const [search, setSearch] = useState("")
+  const [focus, setFocus] = useState("")
+
+  const handleFocusOn = e => {
+    setFocus("has-focus")
+    console.log(`Focus is... ${focus}`)
+  }
+
+  const handleFocusOut = e => {
+    setFocus("")
+    console.log(`Focus is... ${focus}`)
+  }
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -15,33 +26,35 @@ const HomeHeroBody = () => {
     <div className="hero-body">
       <div className="hero-search">
         <div className="columns is-centered">
-          <div className="column is-half has-text-centered">
+          <div className="column is-two-thirds has-text-centered">
             <h2 className="title is-2">Find your ideal property</h2>
             <h3 className="title is-3">With help from the renting community</h3>
           </div>
         </div>
         <div className="columns is-centered">
-          <div className="column is-half">
+          <div className="column is-two-thirds">
             <form onSubmit={handleSubmit}>
               <div className="field has-addons">
                 <p className="control has-icons-left">
-                  <span className="icon">
+                  <span className={`icon ${focus}`}>
                     <FontAwesomeIcon icon={faMapMarkerAlt} />
                   </span>
                 </p>
                 <div className="control">
                   <input
-                    className="input is-primary is-large has-text-centered"
+                    className={`input is-primary is-large ${focus}`}
                     type="text"
                     name="search"
                     placeholder="Enter Postcode"
                     value={search}
+                    onFocus={handleFocusOn}
+                    onBlur={handleFocusOut}
                     onChange={e => setSearch(e.target.value)}
                   />
                 </div>
                 <div className="control">
                   <button
-                    className="button input is-large is-dark"
+                    className={`button input is-large is-dark ${focus}`}
                     type="submit"
                   >
                     Search
